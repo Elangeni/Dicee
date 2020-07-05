ec4eac2f9f28024bf2ea90bf21ac83f42f76c114
Elangeni Yabba <41975353+Elangeni@users.noreply.github.com>
2020-07-04 22:42:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", "images/dice" + randomNumber1 +".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", "images/dice" + randomNumber2 +".png");
+
+if(randomNumber1 > randomNumber2){
+  document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML = "Player 1 Wins!";
+}else if(randomNumber1 < randomNumber2){
+  document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML = "Player 2 Wins!";
+}else{
+  document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML = "Draw!";
+}
